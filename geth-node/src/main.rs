ca29bded3b9939c166fa85b6afc51c84e598f852
YoEight <yo.eight@gmail.com>
2022-09-16 00:16:07 -0400

    Start working on in-memory network implementation.

--- geth-node/src/main.rs
+++ geth-node/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use tokio::sync::{mpsc, oneshot};
 use uuid::Uuid;
 
@@ -11,11 +13,17 @@ pub enum Error {
     Timeout,
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, PartialEq, PartialOrd, Ord, Eq, Hash)]
 pub struct NodeId(Uuid);
 
+impl NodeId {
+    pub fn new() -> Self {
+        NodeId(uuid::Uuid::new_v4())
+    }
+}
+
 struct Msg {
-    target: NodeId,
+    origin: NodeId,
     rpc: Rpc,
 }
 
@@ -63,19 +71,16 @@ pub struct Entry {
 }
 
 pub struct Network {
+    origin: NodeId,
     inner: mpsc::UnboundedSender<Msg>,
 }
 
 impl Network {
-    pub async fn request_vote(
-        &self,
-        target: NodeId,
-        args: RequestVote,
-    ) -> crate::Result<RequestVoteResponse> {
+    pub async fn request_vote(&self, args: RequestVote) -> crate::Result<RequestVoteResponse> {
         let (resp, result) = oneshot::channel();
 
         let _ = self.inner.send(Msg {
-            target,
+            origin: self.origin,
             rpc: Rpc::RequestVote { args, resp },
         });
 
@@ -90,10 +95,70 @@ impl Network {
         let (resp, result) = oneshot::channel();
 
         let _ = self.inner.send(Msg {
-            target,
+            origin: self.origin,
             rpc: Rpc::AppendEntries { args, resp },
         });
 
         result.await.expect("fatal error")
+    }
+}
+
+struct State {
+    current_term: u64,
+    voted_for: Option<NodeId>,
+    log: Vec<Entry>,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            current_term: 0,
+            voted_for: None,
+            log: Vec::new(),
+        }
+    }
+}
+
+async fn raft_node(network: Network, mut state: State, mut mailbox: mpsc::UnboundedReceiver<Msg>) {
+    let mut commit_index = 0u64;
+    let mut last_applied = 0u64;
+
+    while let Some(msg) = mailbox.recv().await {
+        match msg.rpc {
+            Rpc::AppendEntries { args, resp } => {}
+            Rpc::RequestVote { args, resp } => {}
+        }
+    }
+}
+
+type Nodes = HashMap<NodeId, mpsc::UnboundedSender<Msg>>;
+
+fn in_memory_network(node_count: usize) {
+    let mut nodes = HashMap::with_capacity(node_count);
+    let (net_send, socket) = mpsc::unbounded_channel();
+
+    for _ in std::iter::repeat(()).take(node_count) {
+        let node_id = NodeId::new();
+        let (sender, mailbox) = mpsc::unbounded_channel();
+
+        let network = Network {
+            origin: node_id,
+            inner: net_send.clone(),
+        };
+
+        tokio::spawn(raft_node(network, State::new(), mailbox));
+
+        nodes.insert(node_id, sender);
+    }
+
+    tokio::spawn(in_memory_loop(nodes, socket));
+}
+
+async fn in_memory_loop(nodes: Nodes, mut socket: mpsc::UnboundedReceiver<Msg>) {
+    while let Some(msg) = socket.recv().await {
+        match &msg.rpc {
+            Rpc::AppendEntries { args, resp } => {}
+            Rpc::RequestVote { args, resp } => {}
+        }
     }
 }
