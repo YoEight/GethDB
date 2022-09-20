ed5e987e06b551f03e71519425a34a2ca6fbc6b4
YoEight <yo.eight@gmail.com>
2022-09-20 00:17:31 -0400

    More stuff added.

--- geth-node/src/main.rs
+++ geth-node/src/main.rs
@@ -3,6 +3,7 @@ use std::{
     time::{Duration, Instant},
 };
 
+use rand::{rngs::SmallRng, seq::IteratorRandom, SeedableRng};
 use tokio::{
     select,
     sync::{mpsc, oneshot, watch},
@@ -66,8 +67,8 @@ pub struct AppendEntriesResponse {
 pub struct RequestVote {
     pub term: u64,
     pub candidate_id: NodeId,
-    pub last_log_index: usize,
-    pub last_log_term: usize,
+    pub last_log_index: u64,
+    pub last_log_term: u64,
 }
 
 pub struct RequestVoteResponse {
@@ -76,8 +77,8 @@ pub struct RequestVoteResponse {
 }
 
 pub struct Entry {
-    pub index: usize,
-    pub data: usize,
+    pub index: u64,
+    pub data: u64,
 }
 
 pub struct Network {
@@ -102,38 +103,43 @@ impl Network {
             payload: Payload::Resp(resp),
         });
     }
-/*    pub async fn request_vote(&self, target: NodeId, args: RequestVote) -> crate::Result<RequestVoteResponse> {
-        let (resp, result) = oneshot::channel();
+    /*    pub async fn request_vote(&self, target: NodeId, args: RequestVote) -> crate::Result<RequestVoteResponse> {
+            let (resp, result) = oneshot::channel();
 
-        let _ = self.inner.send(Msg {
-            target,
-            rpc: Rpc::RequestVote { args, resp },
-        });
+            let _ = self.inner.send(Msg {
+                target,
+                rpc: Rpc::RequestVote { args, resp },
+            });
 
-        result.await.expect("fatal error")
-    }
+            result.await.expect("fatal error")
+        }
 
-    pub async fn append_entries(
-        &self,
-        target: NodeId,
-        args: AppendEntries,
-    ) -> crate::Result<AppendEntriesResponse> {
-        let (resp, result) = oneshot::channel();
+        pub async fn append_entries(
+            &self,
+            target: NodeId,
+            args: AppendEntries,
+        ) -> crate::Result<AppendEntriesResponse> {
+            let (resp, result) = oneshot::channel();
 
-        let _ = self.inner.send(Msg {
-            target,
-            rpc: Rpc::AppendEntries { args, resp },
-        });
+            let _ = self.inner.send(Msg {
+                target,
+                rpc: Rpc::AppendEntries { args, resp },
+            });
 
-        result.await.expect("fatal error")
-    }
-*/
+            result.await.expect("fatal error")
+        }
+    */
     pub fn origin(&self) -> NodeId {
         self.origin
     }
 
     pub fn other_nodes(&self) -> Vec<NodeId> {
-        self.cluster.borrow().iter().copied().filter(|id| id != &self.origin).collect()
+        self.cluster
+            .borrow()
+            .iter()
+            .copied()
+            .filter(|id| id != &self.origin)
+            .collect()
     }
 }
 
@@ -160,15 +166,20 @@ enum Status {
     Candidate,
 }
 
+fn generate_timeout(rng: &mut SmallRng, low: u64, high: u64) -> Duration {
+    Duration::from_millis((low..high).choose(rng).unwrap())
+}
+
 async fn raft_node(
     network: Network,
     mut state: PersistentState,
     mut mailbox: mpsc::UnboundedReceiver<Msg>,
 ) {
+    let mut rng = SmallRng::from_entropy();
     let mut commit_index = 0u64;
     let mut last_applied = 0u64;
     let mut status = Status::Follower;
-    let mut timeout_election = Duration::from_millis(150);
+    let mut timeout_election = generate_timeout(&mut rng, 150, 300);
     let mut clock = tokio::time::interval(Duration::from_millis(30));
     let mut last_recv_msg = Instant::now();
 
@@ -178,14 +189,32 @@ async fn raft_node(
                 match msg.payload {
                     Payload::Req(req) => {
                         match req {
-                            Req::AppendEntries(args) => {}
-                            Req::RequestVote(args) => {
-                                if args.term < state.current_term {
-                                    network.send_resp(msg.origin, Resp::RequestVote(RequestVoteResponse {
-                                        term: state.current_term,
-                                        vote_granted: false,
-                                    }));
+                            Req::AppendEntries(args) => {
+                                if args.term >= state.current_term && status == Status::Candidate {
+                                    status = Status::Follower;
                                 }
+
+                                if status == Status::Candidate {
+                                    // We ignore the request because it comes from a node that got
+                                    // recently demoted.
+                                    continue;
+                                }
+                            }
+                            Req::RequestVote(args) => {
+                                let last_log_index = state.log.last().map(|e| e.index).unwrap_or(0);
+
+                                let vote_granted = if args.term < state.current_term {
+                                    false
+                                } else if (state.voted_for.is_none() || state.voted_for == Some(args.candidate_id)) && args.last_log_index >= last_log_index {
+                                    true
+                                } else {
+                                    false
+                                };
+
+                                network.send_resp(msg.origin, Resp::RequestVote(RequestVoteResponse {
+                                    term: state.current_term,
+                                    vote_granted,
+                                }));
                             }
                         }
                     }
@@ -217,7 +246,10 @@ async fn raft_node(
 type Nodes = HashMap<NodeId, mpsc::UnboundedSender<Msg>>;
 
 fn in_memory_network(node_count: usize) {
-    let node_ids = std::iter::repeat(()).take(node_count).map(|_| NodeId::new()).collect::<Vec<_>>();
+    let node_ids = std::iter::repeat(())
+        .take(node_count)
+        .map(|_| NodeId::new())
+        .collect::<Vec<_>>();
     let mut nodes = HashMap::with_capacity(node_count);
     let (net_send, socket) = mpsc::unbounded_channel();
     let (cluster_changes, cluster_recv) = watch::channel(node_ids.clone());
