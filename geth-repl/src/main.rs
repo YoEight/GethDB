3c3d75a30818f483281a3f22c57496682394173f
YoEight <yo.eight@gmail.com>
2023-01-31 22:30:46 -0500

    Implement geth-repl prompt.

--- geth-repl/src/main.rs
+++ geth-repl/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> std::io::Result<()> {
+    let options = glyph::Options::default();
+    let mut inputs = glyph::in_memory_inputs(options)?;
+
+    while let Some(input) = inputs.next_input()? {
+        match input {
+            glyph::Input::Exit => break,
+
+            glyph::Input::String(s) => {
+                println!(">> {}", s);
+            }
+
+            _ => {}
+        }
+    }
+
+    Ok(())
 }
