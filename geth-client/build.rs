9955cba78ebf2e5f5182ace94db10dffdead32e2
YoEight <yo.eight@gmail.com>
2023-02-01 15:32:15 -0500

    Simpler.

--- geth-client/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .build_server(false)
-        .build_client(true)
-        .compile(&["../protos/streams.proto"], &["../protos/"])?;
-
-    Ok(())
-}
--- geth-node/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .build_server(true)
-        .build_client(true)
-        .compile(&["../protos/streams.proto"], &["../protos/"])?;
-
-    Ok(())
-}
