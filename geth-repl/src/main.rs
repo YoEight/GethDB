61456344fef4c7574a2c5136c5193168df535da8
YoEight <yo.eight@gmail.com>
2023-02-03 22:25:30 -0500

    Start working on the repl that will read from the DB.

--- geth-repl/src/main.rs
+++ geth-repl/src/main.rs
@@ -1,6 +1,10 @@
-fn main() -> std::io::Result<()> {
+use geth_client::Client;
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
     let options = glyph::Options::default();
     let mut inputs = glyph::in_memory_inputs(options)?;
+    let mut client = Client::new("[::1]:2113").await?;
 
     while let Some(input) = inputs.next_input()? {
         match input {
@@ -9,6 +13,8 @@ fn main() -> std::io::Result<()> {
             glyph::Input::String(s) => {
                 println!(">> {}", s);
             }
+
+            glyph::Input::Command { name, params } => {}
 
             _ => {}
         }
