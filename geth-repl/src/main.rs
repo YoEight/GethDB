c6cdc29a3cc12e5267c670169d3c7822048c9569
YoEight <yo.eight@gmail.com>
2023-02-04 23:10:56 -0500

    Implement a nice REPL on top of the GethDB.

--- geth-repl/src/main.rs
+++ geth-repl/src/main.rs
@@ -1,10 +1,11 @@
 use geth_client::Client;
+use structopt::StructOpt;
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     let options = glyph::Options::default();
     let mut inputs = glyph::in_memory_inputs(options)?;
-    let mut client = Client::new("[::1]:2113").await?;
+    // let mut client = Client::new("http://[::1]:2113").await?;
 
     while let Some(input) = inputs.next_input()? {
         match input {
@@ -14,11 +15,44 @@ async fn main() -> eyre::Result<()> {
                 println!(">> {}", s);
             }
 
-            glyph::Input::Command { name, params } => {}
+            glyph::Input::Command { name, params } => {
+                let mut cmd = vec![":".to_string(), name];
+
+                cmd.extend(params);
+
+                match Cmd::from_iter_safe(&cmd) {
+                    Err(e) => {
+                        println!("{}", e);
+                    }
+
+                    Ok(cmd) => match cmd {
+                        Cmd::Exit => break,
+                        Cmd::ReadStream(opts) => {
+                            println!("{:?}", opts);
+                        }
+                    },
+                }
+            }
 
             _ => {}
         }
     }
 
     Ok(())
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = ":")]
+enum Cmd {
+    #[structopt(name = "read", about = "Read a stream")]
+    ReadStream(ReadStream),
+
+    #[structopt(about = "Exit the application")]
+    Exit,
+}
+
+#[derive(StructOpt, Debug)]
+struct ReadStream {
+    #[structopt(long)]
+    stream_name: String,
 }
