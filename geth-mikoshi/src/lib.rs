805c6268e05533cf7906049eb00d0d59ad1f0a04
YoEight <yo.eight@gmail.com>
2023-02-06 08:04:32 -0500

    PUsh init mikoshi impl.

--- geth-mikoshi/src/lib.rs
+++ geth-mikoshi/src/lib.rs
@@ -1,7 +1,34 @@
+use std::collections::HashMap;
+
 use bytes::Bytes;
+use chrono::{DateTime, Utc};
 use eyre::bail;
 use geth_common::{Position, Record};
 use uuid::Uuid;
+
+#[derive(Debug)]
+struct Entry {
+    pub id: Uuid,
+    pub stream: String,
+    pub revision: u64,
+    pub data: Bytes,
+    pub position: Position,
+    pub created: DateTime<Utc>,
+}
+
+#[derive(Default)]
+pub struct Mikoshi {
+    log: Vec<Entry>,
+    indexes: HashMap<String, usize>,
+    revisions: HashMap<String, u64>,
+    log_position: u64,
+}
+
+impl Mikoshi {
+    pub fn in_memory() -> Self {
+        Default::default()
+    }
+}
 
 pub struct MikoshiStream {}
 
