2efdc40aa923380865ef022de0f79d22ae70f3d9
YoEight <yo.eight@gmail.com>
2023-04-03 22:32:56 -0400

    Implement load_index_map function.

--- geth-mikoshi/src/backend/esdb/synchronous/index/map.rs
+++ geth-mikoshi/src/backend/esdb/synchronous/index/map.rs
@@ -1,11 +1,18 @@
 use crate::backend::esdb::synchronous::index::mem_table::MemTable;
+use crate::backend::esdb::synchronous::index::{
+    IndexMap, Key, Midpoint, PTable, PTableFooter, PTableHeader, ENTRY_SIZE, MD5_SIZE,
+    PTABLE_FOOTER_SIZE, PTABLE_HEADER_SIZE,
+};
+use byteorder::{BigEndian, LittleEndian, ReadBytesExt};
+use bytes::{BufMut, Bytes, BytesMut};
 use geth_common::Position;
 use std::fs::OpenOptions;
 use std::io;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Read, Seek, SeekFrom};
 use std::path::PathBuf;
+use uuid::Uuid;
 
-pub fn load_index_map(root: &PathBuf) -> eyre::Result<MemTable> {
+pub fn load_index_map(root: &PathBuf, buffer: &mut BytesMut) -> eyre::Result<IndexMap> {
     let file = OpenOptions::new()
         .read(true)
         .open(root.join("index").join("indexmap"))?;
@@ -13,6 +20,8 @@ pub fn load_index_map(root: &PathBuf) -> eyre::Result<MemTable> {
     let mut file = BufReader::new(file);
     let mut version = 0i32;
     let mut position = Position(0);
+    let mut auto_merge_level = u64::MAX;
+    let mut tables = Vec::new();
 
     for (idx, line) in file.lines().enumerate() {
         let line = line?;
@@ -47,19 +56,117 @@ pub fn load_index_map(root: &PathBuf) -> eyre::Result<MemTable> {
         }
 
         if idx == 3 && version > 1 {
-            // TODO - Read max auto merge level.
+            auto_merge_level = line.parse::<i32>()? as u64;
             continue;
         }
 
         // PTable parsing section
         let splits = line.split(',').collect::<Vec<_>>();
         let level = splits[0].parse::<i32>()?;
-        let position = splits[1].parse::<i32>()?;
+        let order = splits[1].parse::<i32>()?;
         let file = splits[2];
+        let id = file.parse::<Uuid>()?;
         let filepath = root.join("index").join(file);
 
-        // TODO - Parse the PTable here.
+        let mut file = OpenOptions::new().read(true).open(filepath)?;
+        file.seek(SeekFrom::Start(0))?;
+
+        buffer.reserve(PTABLE_HEADER_SIZE);
+        unsafe {
+            buffer.set_len(PTABLE_HEADER_SIZE);
+        }
+
+        file.read_exact(&mut buffer.as_mut()[..PTABLE_HEADER_SIZE])?;
+        let header = PTableHeader::load(buffer.split().freeze())?;
+
+        // TODO - Double check because I'm pretty sure this is not required at all.
+        buffer.reserve(PTABLE_FOOTER_SIZE);
+        unsafe {
+            buffer.set_len(PTABLE_FOOTER_SIZE);
+        }
+
+        file.seek(SeekFrom::End(-((MD5_SIZE + PTABLE_FOOTER_SIZE) as i64)))?;
+        file.read_exact(&mut buffer.as_mut()[..PTABLE_FOOTER_SIZE])?;
+        let footer = PTableFooter::load(buffer.split().freeze())?;
+
+        let file_size = file.metadata()?.len();
+        let entries_size = file_size as usize
+            - PTABLE_HEADER_SIZE
+            - MD5_SIZE
+            - PTABLE_FOOTER_SIZE
+            - footer.cached_midpoint_size();
+
+        if entries_size % ENTRY_SIZE != 0 {
+            eyre::bail!(
+                "Total size of index entries {} is not divisible by index entry size {}",
+                entries_size,
+                ENTRY_SIZE
+            );
+        }
+
+        let entries_count = entries_size / ENTRY_SIZE;
+
+        if entries_count > 0 && footer.cached_midpoints_num < 2 {
+            eyre::bail!(
+                "Less than 2 midpoints cached in PTable. Index entries = {}, Midpoints cached {}",
+                entries_count,
+                footer.cached_midpoints_num
+            );
+        }
+
+        let offset = MD5_SIZE + PTABLE_FOOTER_SIZE + entries_size;
+        file.seek(SeekFrom::End(-(offset as i64)))?;
+
+        let mut file = BufReader::new(file);
+        let mut midpoints = Vec::new();
+
+        for _ in 0..footer.cached_midpoints_num {
+            let revision = file.read_i64::<LittleEndian>()?;
+            let stream = file.read_u64::<LittleEndian>()?;
+            let offset = file.read_i64::<LittleEndian>()?;
+
+            midpoints.push(Midpoint {
+                key: Key {
+                    stream,
+                    revision: revision as u64,
+                },
+                offset: offset as u64,
+            });
+        }
+
+        tables.push(PTable {
+            id,
+            level: level as u64,
+            order,
+            header,
+            footer,
+            midpoints,
+        });
     }
 
-    todo!()
+    // TODO - Double check, pretty sure we don't need to enforce order here.
+    tables.sort_by_key(|p| p.order);
+
+    Ok(IndexMap {
+        version: version as u64,
+        position,
+        auto_merge_level,
+        tables,
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::backend::esdb::synchronous::index::map::load_index_map;
+    use bytes::BytesMut;
+    use std::path::PathBuf;
+
+    #[test]
+    fn load_working_set_index_map() -> eyre::Result<()> {
+        let mut buffer = BytesMut::new();
+        let root = PathBuf::from("./working-set");
+
+        let index_map = load_index_map(&root, &mut buffer)?;
+        Ok(())
+    }
 }
--- geth-mikoshi/src/backend/esdb/synchronous/index/mod.rs
+++ geth-mikoshi/src/backend/esdb/synchronous/index/mod.rs
@@ -1,3 +1,93 @@
+use bytes::Buf;
+use geth_common::Position;
+use uuid::Uuid;
+
 mod entry;
 mod map;
 mod mem_table;
+
+pub const MD5_SIZE: usize = 16;
+pub const PTABLE_HEADER_SIZE: usize = 128;
+pub const PTABLE_FOOTER_SIZE: usize = 128;
+pub const ENTRY_SIZE: usize = 24;
+pub const KEY_SIZE: usize = 16;
+
+#[derive(Copy, Clone, Debug)]
+pub struct Key {
+    pub stream: u64,
+    pub revision: u64,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Midpoint {
+    key: Key,
+    offset: u64,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct PTableHeader {
+    pub version: u8,
+}
+
+impl PTableHeader {
+    pub fn load<B>(mut src: B) -> eyre::Result<Self>
+    where
+        B: Buf,
+    {
+        if src.get_u8() != 1u8 {
+            eyre::bail!("Invalid PTable file");
+        }
+
+        Ok(Self {
+            version: src.get_u8(),
+        })
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct PTableFooter {
+    pub version: u8,
+    pub cached_midpoints_num: usize,
+}
+
+impl PTableFooter {
+    pub fn load<B>(mut src: B) -> eyre::Result<Self>
+    where
+        B: Buf,
+    {
+        if src.get_u8() != 1u8 {
+            eyre::bail!("Invalid PTable file");
+        }
+
+        let version = src.get_u8();
+
+        let cached_midpoints_num = src.get_u32_le() as usize;
+
+        Ok(Self {
+            version,
+            cached_midpoints_num,
+        })
+    }
+
+    pub fn cached_midpoint_size(&self) -> usize {
+        self.cached_midpoints_num * ENTRY_SIZE
+    }
+}
+
+#[derive(Debug)]
+pub struct PTable {
+    pub id: Uuid,
+    pub level: u64,
+    pub order: i32,
+    pub header: PTableHeader,
+    pub footer: PTableFooter,
+    pub midpoints: Vec<Midpoint>,
+}
+
+#[derive(Debug)]
+pub struct IndexMap {
+    pub version: u64,
+    pub position: Position,
+    pub auto_merge_level: u64,
+    pub tables: Vec<PTable>,
+}
