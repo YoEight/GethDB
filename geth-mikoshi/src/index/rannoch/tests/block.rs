2b3749d2ffcd9ce53773a2a546724cdde36a81a7
YoEight <yo.eight@gmail.com>
2023-04-22 21:37:26 -0400

    Add more in_mem block tests.

--- geth-mikoshi/src/index/rannoch/tests/block.rs
+++ geth-mikoshi/src/index/rannoch/tests/block.rs
@@ -1,40 +1,12 @@
 use crate::index::rannoch::block::{Block, Builder, BLOCK_ENTRY_SIZE};
 use crate::index::rannoch::in_mem::InMemStorage;
 use crate::index::rannoch::ss_table::SsTable;
+use crate::index::rannoch::tests::{key_of, position_of, revision_of, test_ss_table, NUM_OF_KEYS};
 use bytes::BytesMut;
-
-const NUM_OF_KEYS: usize = 100;
-
-fn key_of(idx: usize) -> u64 {
-    idx as u64 * 5
-}
-
-fn revision_of(idx: usize) -> u64 {
-    idx as u64 * 42
-}
-
-fn position_of(idx: usize) -> u64 {
-    idx as u64
-}
 
 fn in_mem_generate_block(storage: &mut InMemStorage, table: &mut SsTable) {
     let values = (0..NUM_OF_KEYS).map(|idx| (key_of(idx), revision_of(idx), position_of(idx)));
     storage.sst_put(table, values);
-}
-
-fn generate_block() -> Block {
-    let mut buffer = BytesMut::new();
-    let mut builder = Block::builder(&mut buffer, 10_000);
-
-    for idx in 0..NUM_OF_KEYS {
-        let key = key_of(idx);
-        let revision = revision_of(idx);
-        let position = position_of(idx);
-
-        assert!(builder.add(key, revision, position));
-    }
-
-    builder.build()
 }
 
 #[test]
@@ -96,69 +68,14 @@ fn test_in_mem_block_build_all() {
 }
 
 #[test]
-fn test_block_build_single_key() {
-    let mut buffer = BytesMut::new();
-    let mut builder = Block::builder(&mut buffer, 26);
+fn test_in_mem_block_find_key() {
+    let mut storage = InMemStorage::default();
+    let mut table = test_ss_table();
 
-    assert!(builder.add(233, 2333, 23333));
+    in_mem_generate_block(&mut storage, &mut table);
 
-    let block = builder.build();
-    assert_eq!(1, block.len());
+    let block = storage.sst_read_block(&table, 0).unwrap();
 
-    let entry = block.read_entry(0);
-    assert!(entry.is_some());
-
-    let entry = entry.unwrap();
-    assert_eq!(233, entry.key);
-    assert_eq!(2333, entry.revision);
-    assert_eq!(23333, entry.position);
-}
-
-#[test]
-fn test_block_build_full() {
-    let mut buffer = BytesMut::new();
-    let mut builder = Block::builder(&mut buffer, 26);
-
-    assert!(builder.add(1, 1, 1));
-    assert!(!builder.add(2, 2, 2));
-
-    let block = builder.build();
-    assert_eq!(1, block.len());
-}
-
-#[test]
-fn test_block_build_all() {
-    let block = generate_block();
-
-    for idx in 0..NUM_OF_KEYS {
-        let entry = block.read_entry(idx).unwrap();
-
-        assert_eq!(key_of(idx), entry.key);
-        assert_eq!(revision_of(idx), entry.revision);
-        assert_eq!(position_of(idx), entry.position);
-    }
-}
-
-#[test]
-fn test_block_encoding() {
-    let mut buffer = BytesMut::new();
-    let block = generate_block();
-    let encoded = block.encode(&mut buffer);
-    let decoded_block = Block::decode(encoded);
-
-    for idx in 0..NUM_OF_KEYS {
-        let entry_1 = block.read_entry(idx).unwrap();
-        let entry_2 = decoded_block.read_entry(idx).unwrap();
-
-        assert_eq!(entry_1.key, entry_2.key);
-        assert_eq!(entry_1.revision, entry_2.revision);
-        assert_eq!(entry_1.position, entry_2.position);
-    }
-}
-
-#[test]
-fn test_block_find_key() {
-    let block = generate_block();
     for i in 0..NUM_OF_KEYS {
         let entry = block
             .find_entry(key_of(i), revision_of(i))
--- geth-mikoshi/src/index/rannoch/tests/mod.rs
+++ geth-mikoshi/src/index/rannoch/tests/mod.rs
@@ -1,4 +1,36 @@
+use crate::index::rannoch::in_mem::InMemStorage;
+use crate::index::rannoch::ss_table::{BlockMetas, SsTable};
+use uuid::Uuid;
+
 mod block;
 mod mem_table;
 mod merge;
 mod ss_table;
+
+pub const NUM_OF_KEYS: usize = 100;
+
+pub fn key_of(idx: usize) -> u64 {
+    idx as u64 * 5
+}
+
+pub fn revision_of(idx: usize) -> u64 {
+    idx as u64 * 42
+}
+
+pub fn position_of(idx: usize) -> u64 {
+    idx as u64
+}
+
+pub fn test_ss_table() -> SsTable {
+    SsTable {
+        id: Uuid::nil(),
+        data: Default::default(),
+        metas: BlockMetas::new(Default::default()),
+    }
+}
+
+pub fn in_mem_generate_block(storage: &mut InMemStorage) {
+    let mut table = test_ss_table();
+    let values = (0..NUM_OF_KEYS).map(|idx| (key_of(idx), revision_of(idx), position_of(idx)));
+    storage.sst_put(&mut table, values);
+}
