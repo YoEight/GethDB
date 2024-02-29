67eb2a7dc739023e9e6f6cdc3374cd4f67489e9e
YoEight <yo.eight@gmail.com>
2024-02-29 00:02:37 -0500

    Replication seems almost done. Now I need to handle user requests.

--- geth-consensus/src/lib.rs
+++ geth-consensus/src/lib.rs
@@ -1,5 +1,5 @@
 use std::cmp::min;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::hash::Hash;
 use std::io;
 use std::time::{Duration, Instant};
@@ -106,6 +106,26 @@ impl TimeRange {
     }
 }
 
+pub struct Replica<Id> {
+    id: Id,
+    next_index: u64,
+    match_index: u64,
+    // When sending entries to replica represents the last index of the batch. If the replication
+    // was successful, that value will be used to update the next_index value.
+    batch_end_index: u64,
+}
+
+impl<Id> Replica<Id> {
+    pub fn new(id: Id) -> Self {
+        Self {
+            id,
+            next_index: 0,
+            match_index: 0,
+            batch_end_index: 0,
+        }
+    }
+}
+
 fn run_raft_app<NodeId, Storage, Command, R, S, D>(
     node_id: NodeId,
     seeds: Vec<NodeId>,
@@ -122,10 +142,18 @@ fn run_raft_app<NodeId, Storage, Command, R, S, D>(
     D: CommandDispatch<Command = Command>,
 {
     let mut tally = HashSet::<NodeId>::new();
+    let mut replicas = HashMap::new();
     let mut commit_index = 0u64;
+    let mut last_applied = 0u64;
     let mut voted_for = None;
     let mut time_tracker = Instant::now();
     let mut election_timeout = time_range.update_timeout();
+
+    for seed_id in &seeds {
+        let state = Replica::new(seed_id.clone());
+        replicas.insert(seed_id.clone(), state);
+    }
+
     let mut state = if seeds.is_empty() {
         State::Leader
     } else {
@@ -292,13 +320,58 @@ fn run_raft_app<NodeId, Storage, Command, R, S, D>(
                     // If the cluster reached quorum
                     if tally.len() + 1 >= (seeds.len() + 1) / 2 {
                         state = State::Leader;
-                        tally.clear();
+
+                        let last_index = storage.last_entry().map(|e| e.index).unwrap_or_default();
+                        for replica in replicas.values_mut() {
+                            replica.next_index = last_index + 1;
+                            replica.match_index = 0;
+                        }
                     }
                 }
             }
 
-            Msg::EntriesAppended(_) => {}
-            Msg::Command(_) => {}
+            Msg::EntriesAppended(args) => {
+                if state != State::Leader {
+                    continue;
+                }
+
+                if let Some(replica) = replicas.get_mut(&args.node_id) {
+                    if args.success {
+                        replica.match_index = replica.batch_end_index;
+                        replica.next_index = replica.batch_end_index + 1;
+
+                        let mut lowest_replicated_index = u64::MAX;
+                        for replica in replicas.values() {
+                            lowest_replicated_index =
+                                min(lowest_replicated_index, replica.match_index);
+                        }
+
+                        // TODO - If we have pending user append commands, we reports all operations
+                        // lesser or equal to `lowest_replicated_index` completed successfully.
+
+                        commit_index = lowest_replicated_index;
+                    } else {
+                        // FIXME - This is the simplest way of handling this. On large dataset, it
+                        // could be beneficial for the replica to actually send an hint of where
+                        // its log actually is.
+                        replica.next_index = replica.next_index.saturating_sub(1);
+                    }
+                }
+            }
+
+            Msg::Command(args) => {
+                // If we are dealing with a write command but are not the leader of the cluster,
+                // we must refuse to serve the command.
+                //
+                // NOTE - Depending on the use case, it might not be ok to serve read command if
+                // we are not the leader either. It the node is lagging behind replication-wise,
+                // the user might get different view of the data whether they are reading from the
+                // leader node or not.
+                if !args.is_read() && state != State::Leader {
+                    // TODO - Reject the command.
+                }
+            }
+
             Msg::Tick => {}
             Msg::Shutdown => {
                 break;
