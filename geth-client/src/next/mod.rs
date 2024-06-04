1937437ab229184bd77109372900d0bcdb1cbfcf
YoEight <yo.eight@gmail.com>
2024-06-04 11:09:12 -0400

    pending request impl

--- geth-client/src/next/mod.rs
+++ geth-client/src/next/mod.rs
@@ -1,13 +1,19 @@
 use tokio::sync::mpsc::UnboundedReceiver;
+use uuid::Uuid;
 
-use geth_common::{AppendStream, AppendStreamCompleted, DeleteStream, DeleteStreamCompleted, ReadStream, StreamRead, Subscribe, SubscriptionEvent};
+use geth_common::{AppendStream, AppendStreamCompleted, DeleteStream, DeleteStreamCompleted, EndPoint, ReadStream, StreamRead, Subscribe, SubscriptionEvent};
 
 enum Msg {
     Request(Request),
     Response(Response),
 }
 
-enum Request {
+pub struct Request {
+    pub correlation: Uuid,
+    pub operation: Operation,
+}
+
+enum Operation {
     AppendStream(AppendStream),
     DeleteStream(DeleteStream),
     ReadStream(ReadStream),
@@ -21,21 +27,21 @@ enum Response {
     DeleteStreamCompleted(DeleteStreamCompleted),
 }
 
-async fn multiplex_loop(mut receiver: UnboundedReceiver<Msg>) {
+async fn multiplex_loop(mut endpoint: EndPoint, mut receiver: UnboundedReceiver<Msg>) {
     while let Some(msg) = receiver.recv().await {
         match msg {
             Msg::Request(req) => {
-                match req {
-                    Request::AppendStream(req) => {
+                match req.operation {
+                    Operation::AppendStream(req) => {
                         // handle append stream request
                     }
-                    Request::DeleteStream(req) => {
+                    Operation::DeleteStream(req) => {
                         // handle delete stream request
                     }
-                    Request::ReadStream(req) => {
+                    Operation::ReadStream(req) => {
                         // handle read stream request
                     }
-                    Request::Subscribe(req) => {
+                    Operation::Subscribe(req) => {
                         // handle subscribe request
                     }
                 }
