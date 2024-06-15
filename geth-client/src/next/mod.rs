a623a36c5a1d9b061e3033e9155fd0f4e651018a
YoEight <yo.eight@gmail.com>
2024-06-15 00:41:14 -0400

    working on setting the new connection code on the client side.

--- geth-client/src/next/mod.rs
+++ geth-client/src/next/mod.rs
@@ -1,7 +1,16 @@
-use tokio::sync::mpsc::UnboundedReceiver;
+use futures_util::StreamExt;
+use tokio::sync::{mpsc, oneshot};
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
+use tonic::codegen::tokio_stream::wrappers::UnboundedReceiverStream;
+use tonic::transport::Uri;
 use uuid::Uuid;
 
-use geth_common::{AppendStream, AppendStreamCompleted, DeleteStream, DeleteStreamCompleted, EndPoint, ReadStream, StreamRead, Subscribe, SubscriptionEvent};
+use geth_common::{
+    AppendStream, AppendStreamCompleted, DeleteStream, DeleteStreamCompleted, EndPoint, ReadStream,
+    StreamRead, Subscribe, SubscriptionEvent,
+};
+use geth_common::generated::next::protocol::{operation_out, OperationIn};
+use geth_common::generated::next::protocol::protocol_client::ProtocolClient;
 
 enum Msg {
     Request(Request),
@@ -11,6 +20,7 @@ enum Msg {
 pub struct Request {
     pub correlation: Uuid,
     pub operation: Operation,
+    pub resp: oneshot::Sender<Response>,
 }
 
 enum Operation {
@@ -20,14 +30,84 @@ enum Operation {
     Subscribe(Subscribe),
 }
 
-enum Response {
+struct Response {
+    correlation: Uuid,
+    response: Reply,
+}
+
+enum Reply {
     AppendStreamCompleted(AppendStreamCompleted),
     StreamRead(StreamRead),
     SubscriptionEvent(SubscriptionEvent),
     DeleteStreamCompleted(DeleteStreamCompleted),
 }
 
+type Connection = UnboundedSender<OperationIn>;
+type Mailbox = UnboundedSender<Msg>;
+
+async fn set_up_connection(uri: Uri, mailbox: Mailbox) -> eyre::Result<Connection> {
+    let mut client = ProtocolClient::connect(uri).await?;
+    let (connection, stream_request) = mpsc::unbounded_channel();
+
+    let mut stream_response = client
+        .multiplex(UnboundedReceiverStream::new(stream_request))
+        .await?
+        .into_inner();
+
+    tokio::spawn(async move {
+        while let Some(reply) = stream_response.next().await {
+            match reply {
+                Err(e) => {
+                    tracing::error!("Error receiving response: {:?}", e);
+                    break;
+                }
+
+                Ok(reply) => match reply.operation.unwrap() {
+                    operation_out::Operation::AppendCompleted(resp) => {
+                        // mailbox.send(Msg::Response(Response {
+                        //     correlation: resp.correlation,
+                        //     response: Reply::AppendStreamCompleted(resp),
+                        // }));
+                    }
+                    operation_out::Operation::StreamRead(resp) => {
+                        // mailbox.send(Msg::Response(Response {
+                        //     correlation: resp.correlation,
+                        //     response: Reply::StreamRead(resp),
+                        // }));
+                    }
+                    operation_out::Operation::SubscriptionEvent(resp) => {
+                        // mailbox.send(Msg::Response(Response {
+                        //     correlation: resp.correlation,
+                        //     response: Reply::SubscriptionEvent(resp),
+                        // }));
+                    }
+                    operation_out::Operation::DeleteCompleted(resp) => {
+                        // mailbox.send(Msg::Response(Response {
+                        //     correlation: resp.correlation,
+                        //     response: Reply::DeleteStreamCompleted(resp),
+                        // }));
+                    }
+                },
+            }
+        }
+    });
+
+    Ok(connection)
+}
+
 async fn multiplex_loop(mut endpoint: EndPoint, mut receiver: UnboundedReceiver<Msg>) {
+    let uri = format!("http://{}:{}", endpoint.host, endpoint.port)
+        .parse::<Uri>()
+        .unwrap();
+    let mut client = ProtocolClient::connect(uri).await.unwrap();
+    let (mailbox, stream_request) = mpsc::unbounded_channel();
+
+    let stream_response = client
+        .multiplex(UnboundedReceiverStream::new(stream_request))
+        .await
+        .unwrap()
+        .into_inner();
+
     while let Some(msg) = receiver.recv().await {
         match msg {
             Msg::Request(req) => {
