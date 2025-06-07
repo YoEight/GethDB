8bd043ec30ec22918e7a38a76131933f785d7d46
YoEight <yo.eight@gmail.com>
2025-06-07 00:12:02 -0400

    we are almost there

--- geth-engine/src/process/grpc/protocol.rs
+++ geth-engine/src/process/grpc/protocol.rs
@@ -1,22 +1,21 @@
 use std::collections::VecDeque;
 
 use futures::{pin_mut, Stream, StreamExt};
+use geth_common::protocol::protocol_server::Protocol;
+use geth_common::protocol::{self, SubscribeResponse};
 use geth_mikoshi::hashing::mikoshi_hash;
 use tokio::select;
 use tokio::sync::mpsc::{self, unbounded_channel};
 use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 use tonic::codegen::tokio_stream::wrappers::UnboundedReceiverStream;
-use tonic::{Request, Response, Status};
 
-use geth_common::generated::protocol;
-use geth_common::generated::protocol::protocol_server::Protocol;
 use geth_common::{
-    AppendStream, Direction, GetProgramError, Operation, OperationIn, OperationOut, ProgramKilled,
-    ProgramListed, ProgramObtained, ReadStream, ReadStreamCompleted, ReadStreamResponse, Record,
-    Reply, Subscribe, SubscribeToStream, SubscriptionConfirmation, SubscriptionEvent,
-    UnsubscribeReason,
+    AppendStream, DeleteStream, Direction, GetProgramError, ListPrograms, Operation, OperationIn,
+    OperationOut, ProgramKilled, ProgramListed, ProgramObtained, ReadStream, ReadStreamCompleted,
+    ReadStreamResponse, Record, Reply, Subscribe, SubscribeToStream, SubscriptionConfirmation,
+    SubscriptionEvent, UnsubscribeReason,
 };
-use uuid::Uuid;
+use tonic::{Request, Response, Status};
 
 use crate::process::reading::ReaderClient;
 use crate::process::subscription::SubscriptionClient;
@@ -111,26 +110,143 @@ impl Protocol for ProtocolImpl {
 
     async fn delete_stream(
         &self,
-        _request: Request<protocol::DeleteStreamRequest>,
+        request: Request<protocol::DeleteStreamRequest>,
     ) -> Result<Response<protocol::DeleteStreamResponse>, Status> {
-        // Not yet implemented
-        Err(Status::unimplemented("delete_stream is not implemented"))
+        let params: DeleteStream = request.into_inner().into();
+
+        match self
+            .writer
+            .delete(params.stream_name, params.expected_revision)
+            .await
+        {
+            Err(e) => Err(Status::internal(e.to_string())),
+            Ok(result) => Ok(Response::new(result.into())),
+        }
     }
 
     async fn subscribe(
         &self,
-        _request: Request<protocol::SubscribeRequest>,
+        request: Request<protocol::SubscribeRequest>,
     ) -> Result<Response<Self::SubscribeStream>, Status> {
-        // Not yet implemented
-        Err(Status::unimplemented("subscribe is not implemented"))
+        let (sender, recv) = unbounded_channel::<Result<SubscribeResponse, Status>>();
+
+        match request.into_inner().into() {
+            Subscribe::ToStream(params) => {
+                match CatchupSubscription::init(self, params).await {
+                    Err(e) => return Err(Status::internal(e.to_string())),
+                    Ok(catchup) => {
+                        if let Some(mut catchup) = catchup {
+                            tokio::spawn(async move {
+                                loop {
+                                    match catchup.next().await {
+                                        Err(e) => {
+                                            let _ =
+                                                sender.send(Err(Status::internal(e.to_string())));
+
+                                            break;
+                                        }
+
+                                        Ok(event) => {
+                                            if let Some(event) = event {
+                                                if sender.send(Ok(event.into())).is_err() {
+                                                    tracing::debug!(
+                                                        stream = catchup.params.stream_name,
+                                                        "user disconnected from catchup subscription"
+                                                    );
+
+                                                    break;
+                                                }
+                                            } else {
+                                                tracing::debug!(
+                                                    stream = catchup.params.stream_name,
+                                                    "server ended catchup subscription"
+                                                );
+
+                                                let _ = sender.send(Ok(
+                                                    SubscriptionEvent::Unsubscribed(
+                                                        UnsubscribeReason::Server,
+                                                    )
+                                                    .into(),
+                                                ));
+
+                                                break;
+                                            }
+                                        }
+                                    }
+                                }
+                            });
+                        } else {
+                            return Err(Status::failed_precondition("stream-deleted"));
+                        }
+                    }
+                };
+            }
+
+            Subscribe::ToProgram(params) => {
+                match self
+                    .sub
+                    .subscribe_to_program(&params.name, &params.source)
+                    .await
+                {
+                    Err(e) => return Err(Status::internal(e.to_string())),
+                    Ok(mut stream) => {
+                        tokio::spawn(async move {
+                            loop {
+                                match stream.next().await {
+                                    Err(e) => {
+                                        let _ = sender.send(Err(Status::internal(e.to_string())));
+                                        break;
+                                    }
+
+                                    Ok(event) => {
+                                        if let Some(event) = event {
+                                            if sender
+                                                .send(Ok(
+                                                    SubscriptionEvent::EventAppeared(event).into()
+                                                ))
+                                                .is_err()
+                                            {
+                                                tracing::debug!(
+                                                    name = params.name,
+                                                    "user disconnected from catchup subscription"
+                                                );
+
+                                                break;
+                                            }
+                                        } else {
+                                            tracing::debug!(
+                                                name = params.name,
+                                                "server ended program subscription"
+                                            );
+
+                                            let _ =
+                                                sender.send(Ok(SubscriptionEvent::Unsubscribed(
+                                                    UnsubscribeReason::Server,
+                                                )
+                                                .into()));
+
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        };
+
+        Ok(Response::new(UnboundedReceiverStream::new(recv)))
     }
 
     async fn list_programs(
         &self,
-        _request: Request<protocol::ListProgramsRequest>,
+        _: Request<protocol::ListProgramsRequest>,
     ) -> Result<Response<protocol::ListProgramsResponse>, Status> {
-        // Not yet implemented
-        Err(Status::unimplemented("list_programs is not implemented"))
+        match self.sub.list_programs().await {
+            Err(e) => Err(Status::internal(e.to_string())),
+            Ok(programs) => Ok(Response::new(ProgramListed { programs }.into())),
+        }
     }
 
     async fn program_stats(
@@ -527,146 +643,124 @@ enum Msg {
 //     }
 // }
 
-// struct CatchupSubscription {
-//     params: SubscribeToStream,
-//     catching_up: bool,
-//     history: VecDeque<Record>,
-//     end_revision: u64,
-//     done: bool,
-//     correlation: Uuid,
-//     read_stream: crate::process::reading::Streaming,
-//     sub_stream: crate::process::subscription::Streaming,
-// }
+struct CatchupSubscription {
+    params: SubscribeToStream,
+    catching_up: bool,
+    history: VecDeque<Record>,
+    end_revision: u64,
+    done: bool,
+    read_stream: crate::process::reading::Streaming,
+    sub_stream: crate::process::subscription::Streaming,
+}
 
-// impl CatchupSubscription {
-//     async fn init(
-//         internal: &Internal,
-//         correlation: Uuid,
-//         params: SubscribeToStream,
-//     ) -> eyre::Result<Option<Self>> {
-//         let sub_stream = internal
-//             .sub
-//             .subscribe_to_stream(&params.stream_name)
-//             .await?;
+impl CatchupSubscription {
+    async fn init(
+        internal: &ProtocolImpl,
+        params: SubscribeToStream,
+    ) -> eyre::Result<Option<Self>> {
+        let sub_stream = internal
+            .sub
+            .subscribe_to_stream(&params.stream_name)
+            .await?;
 
-//         let mut read_stream = match internal
-//             .reader
-//             .read(
-//                 &params.stream_name,
-//                 params.start,
-//                 Direction::Forward,
-//                 usize::MAX,
-//             )
-//             .await?
-//         {
-//             ReadStreamCompleted::StreamDeleted => {
-//                 return Ok(None);
-//             }
+        let mut read_stream = match internal
+            .reader
+            .read(
+                &params.stream_name,
+                params.start,
+                Direction::Forward,
+                usize::MAX,
+            )
+            .await?
+        {
+            ReadStreamCompleted::StreamDeleted => {
+                return Ok(None);
+            }
 
-//             ReadStreamCompleted::Unexpected(e) => {
-//                 return Err(e);
-//             }
+            ReadStreamCompleted::Success(stream) => stream,
+        };
 
-//             ReadStreamCompleted::Success(stream) => stream,
-//         };
+        let current_revision = internal
+            .index
+            .latest_revision(mikoshi_hash(&params.stream_name))
+            .await?;
 
-//         let current_revision = internal
-//             .index
-//             .latest_revision(mikoshi_hash(&params.stream_name))
-//             .await?;
+        if current_revision.is_deleted() {
+            return Ok(None);
+        }
 
-//         if current_revision.is_deleted() {
-//             return Ok(None);
-//         }
+        let mut end_revision = 0;
 
-//         let mut end_revision = 0;
+        if let Some(revision) = current_revision.revision() {
+            end_revision = revision;
+        } else {
+            read_stream = crate::process::reading::Streaming::empty();
+        }
 
-//         if let Some(revision) = current_revision.revision() {
-//             end_revision = revision;
-//         } else {
-//             read_stream = crate::process::reading::Streaming::empty();
-//         }
+        Ok(Some(Self {
+            params,
+            catching_up: false,
+            history: VecDeque::new(),
+            done: false,
+            end_revision,
+            read_stream,
+            sub_stream,
+        }))
+    }
 
-//         Ok(Some(Self {
-//             params,
-//             catching_up: false,
-//             history: VecDeque::new(),
-//             done: false,
-//             correlation,
-//             end_revision,
-//             read_stream,
-//             sub_stream,
-//         }))
-//     }
+    // CAUTION: a situation where an user is reading very far away from the head of the stream and while that stream is actively being writen on could lead
+    // to uncheck memory usage as everything will be stored in the history buffer.
+    //
+    // TODO: Implement a mechanism to limit the size of the history buffer by implementing a backpressure mechanism.
+    async fn next(&mut self) -> eyre::Result<Option<SubscriptionEvent>> {
+        if self.done {
+            return Ok(None);
+        }
 
-//     // CAUTION: a situation where an user is reading very far away from the head of the stream and while that stream is actively being writen on could lead
-//     // to uncheck memory usage as everything will be stored in the history buffer.
-//     //
-//     // TODO: Implement a mechanism to limit the size of the history buffer by implementing a backpressure mechanism.
-//     async fn next(&mut self) -> eyre::Result<Option<OperationOut>> {
-//         if self.done {
-//             return Ok(None);
-//         }
+        if self.catching_up {
+            loop {
+                select! {
+                    outcome = self.read_stream.next() => {
+                        match outcome {
+                            Err(e) => return Err(e),
+                            Ok(outcome) => if let Some(event) = outcome {
+                                return Ok(Some(SubscriptionEvent::EventAppeared(event)));
+                            } else {
+                                self.catching_up = false;
+                                return Ok(Some(SubscriptionEvent::CaughtUp));
+                            }
+                        }
+                    }
 
-//         if self.catching_up {
-//             loop {
-//                 select! {
-//                     outcome = self.read_stream.next() => {
-//                         match outcome {
-//                             Err(e) => return Err(e),
-//                             Ok(outcome) => if let Some(event) = outcome {
-//                                 return Ok(Some(OperationOut {
-//                                     correlation: self.correlation,
-//                                     reply: Reply::SubscriptionEvent(SubscriptionEvent::EventAppeared(event)),
-//                                 }));
-//                             } else {
-//                                 self.catching_up = false;
-//                                 return Ok(Some(OperationOut {
-//                                     correlation: self.correlation,
-//                                     reply: Reply::SubscriptionEvent(SubscriptionEvent::CaughtUp),
-//                                 }));
-//                             }
-//                         }
-//                     }
+                    outcome = self.sub_stream.next() => {
+                        match outcome {
+                            Err(e) => return Err(e),
+                            Ok(outcome) => if let Some(event) = outcome {
+                                if event.revision <= self.end_revision {
+                                    continue;
+                                }
 
-//                     outcome = self.sub_stream.next() => {
-//                         match outcome {
-//                             Err(e) => return Err(e),
-//                             Ok(outcome) => if let Some(event) = outcome {
-//                                 if event.revision <= self.end_revision {
-//                                     continue;
-//                                 }
+                                self.history.push_back(event);
+                            } else {
+                                self.done = true;
+                                return Ok(None);
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-//                                 self.history.push_back(event);
-//                             } else {
-//                                 self.done = true;
-//                                 return Ok(Some(OperationOut {
-//                                     correlation: self.correlation,
-//                                     reply: Reply::SubscriptionEvent(SubscriptionEvent::Unsubscribed(UnsubscribeReason::Server)),
-//                                 }));
-//                             }
-//                         }
-//                     }
-//                 }
-//             }
-//         }
+        if let Some(event) = self.history.pop_front() {
+            return Ok(Some(SubscriptionEvent::EventAppeared(event)));
+        }
 
-//         if let Some(event) = self.history.pop_front() {
-//             return Ok(Some(OperationOut {
-//                 correlation: self.correlation,
-//                 reply: Reply::SubscriptionEvent(SubscriptionEvent::EventAppeared(event)),
-//             }));
-//         }
+        if let Some(event) = self.sub_stream.next().await? {
+            return Ok(Some(SubscriptionEvent::EventAppeared(event)));
+        }
 
-//         if let Some(event) = self.sub_stream.next().await? {
-//             return Ok(Some(OperationOut {
-//                 correlation: self.correlation,
-//                 reply: Reply::SubscriptionEvent(SubscriptionEvent::EventAppeared(event)),
-//             }));
-//         }
+        self.done = true;
 
-//         self.done = true;
-
-//         Ok(None)
-//     }
-// }
+        Ok(None)
+    }
+}
