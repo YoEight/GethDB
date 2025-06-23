925861836da947fbf9d266803d41569951f508c7
YoEight <yo.eight@gmail.com>
2025-06-23 13:47:14 -0400

    add test_empty_index_does_not_hang

--- geth-engine/src/process/tests/indexing.rs
+++ geth-engine/src/process/tests/indexing.rs
@@ -1,5 +1,9 @@
+use std::usize;
+
 use geth_common::Direction;
 use geth_domain::index::BlockEntry;
+use geth_mikoshi::hashing::mikoshi_hash;
+use uuid::Uuid;
 
 use crate::{process::start_process_manager, Options, RequestContext};
 
@@ -63,6 +67,30 @@ async fn test_last_revision_when_non_existent() -> eyre::Result<()> {
     let revision = client.latest_revision(ctx, 2).await?.revision();
 
     assert!(revision.is_none());
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_empty_index_does_not_hang() -> eyre::Result<()> {
+    let manager = start_process_manager(Options::in_mem()).await?;
+    let client = manager.new_index_client().await?;
+    let ctx = RequestContext::new();
+
+    let stream_name = Uuid::new_v4().to_string();
+
+    let mut streaming = client
+        .read(
+            ctx,
+            mikoshi_hash(&stream_name),
+            0,
+            usize::MAX,
+            Direction::Forward,
+        )
+        .await?
+        .ok()?;
+
+    while let Some(_) = streaming.next().await? {}
 
     Ok(())
 }
