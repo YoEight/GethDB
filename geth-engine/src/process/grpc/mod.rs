90b3bb66126dd336328fe4547ab0fab83c11b601
YoEight <yo.eight@gmail.com>
2025-07-05 20:38:26 -0400

    pending metric middleware impl

--- geth-engine/src/process/grpc/mod.rs
+++ geth-engine/src/process/grpc/mod.rs
@@ -1,4 +1,4 @@
-use std::sync::Arc;
+use std::{pin::Pin, sync::Arc};
 
 use tokio::sync::Notify;
 use tonic::transport::Server;
@@ -7,7 +7,7 @@ use geth_grpc::generated::protocol::protocol_server::ProtocolServer;
 use tracing::instrument;
 
 use crate::{
-    metrics::get_metrics,
+    metrics::{get_metrics, Metrics},
     process::{manager::ManagerClient, Managed, ProcessEnv},
     Options,
 };
@@ -28,7 +28,12 @@ pub async fn start_server(
 
     tracing::info!(%addr, db = options.db, "GethDB is listening",);
 
+    let layer = tower::ServiceBuilder::new()
+        .layer(MetricsLayer)
+        .into_inner();
+
     Server::builder()
+        .layer(layer)
         .add_service(ProtocolServer::new(protocols))
         .serve_with_shutdown(addr, notify.notified())
         .await?;
@@ -61,3 +66,61 @@ async fn shutdown(notify: Arc<Notify>, handle: tokio::task::JoinHandle<eyre::Res
     let _ = handle.await;
     tracing::info!("completed");
 }
+
+#[derive(Clone)]
+struct MetricsLayer;
+
+impl<S> tower::Layer<S> for MetricsLayer {
+    type Service = MetricsMiddleware<S>;
+
+    fn layer(&self, inner: S) -> Self::Service {
+        MetricsMiddleware {
+            metrics: get_metrics(),
+            inner,
+        }
+    }
+}
+
+#[derive(Clone)]
+struct MetricsMiddleware<S> {
+    metrics: Metrics,
+    inner: S,
+}
+
+type BoxFuture<'a, T> = Pin<Box<dyn std::future::Future<Output = T> + Send + 'a>>;
+
+impl<S, ReqBody, ResBody> tower::Service<http::Request<ReqBody>> for MetricsMiddleware<S>
+where
+    S: tower::Service<http::Request<ReqBody>, Response = http::Response<ResBody>>
+        + Clone
+        + Send
+        + 'static,
+    S::Future: Send + 'static,
+    ReqBody: Send + 'static,
+{
+    type Response = S::Response;
+    type Error = S::Error;
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(
+        &mut self,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: http::Request<ReqBody>) -> Self::Future {
+        // See: https://docs.rs/tower/latest/tower/trait.Service.html#be-careful-when-cloning-inner-services
+        let clone = self.inner.clone();
+        let metrics = self.metrics.clone();
+        let mut inner = std::mem::replace(&mut self.inner, clone);
+
+        Box::pin(async move {
+            let resp = inner.call(req).await?;
+
+            Ok(resp)
+        })
+    }
+}
+
+fn on_status_error(metrics: &Metrics, status: tonic::Status) {}
