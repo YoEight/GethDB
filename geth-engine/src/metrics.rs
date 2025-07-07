7ee6b1a9af801e1a1e539a8c1dd1333e45eafa4a
YoEight <yo.eight@gmail.com>
2025-07-06 23:32:30 -0400

    complete but now need to test it with prometheus

--- geth-engine/src/metrics.rs
+++ geth-engine/src/metrics.rs
@@ -1,25 +1,39 @@
+use std::{
+    sync::{mpsc, Arc, RwLock},
+    thread,
+    time::Duration,
+};
+
 use geth_mikoshi::wal::{LogEntries, LogEntry};
-use opentelemetry::metrics::{Counter, Histogram, UpDownCounter};
+use opentelemetry::metrics::{Counter, Histogram, ObservableGauge, UpDownCounter};
+use sysinfo::{CpuRefreshKind, MemoryRefreshKind, RefreshKind, System};
 use tokio::sync::OnceCell;
 
 #[derive(Debug, Clone)]
 pub struct Metrics {
-    pub programs_total: Counter<u64>,
-    pub programs_active_total: UpDownCounter<f64>,
-    pub subscriptions_total: Counter<u64>,
-    pub subscriptions_active_total: UpDownCounter<f64>,
-    pub client_errors_total: Counter<u64>,
-    pub server_errors_total: Counter<u64>,
-    pub read_size_bytes: Histogram<f64>,
-    pub read_entry_total: Counter<u64>,
-    pub read_error_total: Counter<u64>,
-    pub index_cache_hits_total: Counter<u64>,
-    pub index_cache_miss_total: Counter<u64>,
-    pub index_read_error_total: Counter<u64>,
-    pub index_write_error_total: Counter<u64>,
-    pub write_size_bytes: Histogram<f64>,
-    pub write_propose_event_total: Counter<u64>,
-    pub write_error_total: Counter<u64>,
+    programs_total: Counter<u64>,
+    programs_active_total: UpDownCounter<f64>,
+    subscriptions_total: Counter<u64>,
+    subscriptions_active_total: UpDownCounter<f64>,
+    client_errors_total: Counter<u64>,
+    server_errors_total: Counter<u64>,
+    read_size_bytes: Histogram<f64>,
+    read_entry_total: Counter<u64>,
+    read_error_total: Counter<u64>,
+    index_cache_hits_total: Counter<u64>,
+    index_cache_miss_total: Counter<u64>,
+    index_read_error_total: Counter<u64>,
+    index_write_error_total: Counter<u64>,
+    write_size_bytes: Histogram<f64>,
+    write_propose_event_total: Counter<u64>,
+    write_error_total: Counter<u64>,
+
+    _total_memory: ObservableGauge<f64>,
+    _used_memory: ObservableGauge<f64>,
+    _total_swap: ObservableGauge<f64>,
+    _used_swap: ObservableGauge<f64>,
+    _cpu_usage: ObservableGauge<f64>,
+    _refresh_thread_send: mpsc::Sender<()>,
 }
 
 impl Metrics {
@@ -99,6 +113,23 @@ pub fn configure_metrics() {
 
 fn init_meter() -> Metrics {
     let meter = opentelemetry::global::meter("geth-engine");
+
+    let refreshes = RefreshKind::nothing()
+        .with_cpu(CpuRefreshKind::nothing().with_cpu_usage())
+        .with_memory(MemoryRefreshKind::everything());
+
+    let sys = System::new_with_specifics(refreshes);
+
+    let sys = Arc::new(RwLock::new(sys));
+    let sys_write = sys.clone();
+    let (_refresh_thread_send, cancellation) = mpsc::channel();
+    thread::spawn(move || refresh_sys_collection(sys_write, cancellation, refreshes));
+
+    let mem_total_sys = sys.clone();
+    let used_mem_sys = sys.clone();
+    let swap_total_sys = sys.clone();
+    let used_swap_sys = sys.clone();
+    let cpu_usage_sys = sys.clone();
 
     Metrics {
         programs_total: meter
@@ -196,5 +227,78 @@ fn init_meter() -> Metrics {
             .with_description("Total number of active subscriptions")
             .with_unit("subscriptions")
             .build(),
+
+        _total_memory: meter
+            .f64_observable_gauge("geth_sys_memory_total")
+            .with_description("Total system memory")
+            .with_unit("bytes")
+            .with_callback(move |inst| {
+                let sys = mem_total_sys.read().unwrap();
+                let total_memory = sys.total_memory() as f64;
+                inst.observe(total_memory, &[]);
+            })
+            .build(),
+
+        _used_memory: meter
+            .f64_observable_gauge("geth_sys_memory_used_bytes")
+            .with_description("Used system memory")
+            .with_unit("bytes")
+            .with_callback(move |inst| {
+                let sys = used_mem_sys.read().unwrap();
+                let used_memory = sys.used_memory() as f64;
+                inst.observe(used_memory, &[]);
+            })
+            .build(),
+
+        _total_swap: meter
+            .f64_observable_gauge("geth_sys_swap_total")
+            .with_description("Total system swap memory")
+            .with_unit("bytes")
+            .with_callback(move |inst| {
+                let sys = swap_total_sys.read().unwrap();
+                let total_swap = sys.total_swap() as f64;
+                inst.observe(total_swap, &[]);
+            })
+            .build(),
+
+        _used_swap: meter
+            .f64_observable_gauge("geth_sys_swap_used_bytes")
+            .with_description("Used system swap memory")
+            .with_unit("bytes")
+            .with_callback(move |inst| {
+                let sys = used_swap_sys.read().unwrap();
+                let used_swap = sys.used_swap() as f64;
+                inst.observe(used_swap, &[]);
+            })
+            .build(),
+
+        _cpu_usage: meter
+            .f64_observable_gauge("geth_sys_cpu_usage")
+            .with_description("CPU usage percentage")
+            .with_unit("percent")
+            .with_callback(move |inst| {
+                let sys = cpu_usage_sys.read().unwrap();
+                let cpu_usage = sys.global_cpu_usage() as f64;
+                inst.observe(cpu_usage, &[]);
+            })
+            .build(),
+
+        _refresh_thread_send,
+    }
+}
+
+fn refresh_sys_collection(
+    handle: Arc<RwLock<System>>,
+    cancellation: mpsc::Receiver<()>,
+    refreshes: RefreshKind,
+) {
+    loop {
+        if cancellation.try_recv().is_err() {
+            break;
+        }
+
+        thread::sleep(Duration::from_secs(10));
+        let mut sys = handle.write().unwrap();
+        sys.refresh_specifics(refreshes);
     }
 }
